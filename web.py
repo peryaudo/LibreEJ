bcaff7f5ae6c67743f528e453682da06dd509196
Tetsui Ohkubo <peryaudo@gmail.com>
2021-09-18 20:46:27 +0900

    Add search to web.py

--- web.py
+++ web.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_from_directory
+import csv
+from collections import namedtuple
+
+Article = namedtuple('Article', ['heading', 'image'])
+
+with open('result/index.csv', 'r') as f:
+    articles = list(map(Article._make, csv.reader(f)))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/search')
+def search():
+    q = request.args.get('q')
+    result = filter(lambda article: q in article.heading, articles)
+    return render_template('search.html', q=q, result=result)
+
+@app.route('/images/<path:path>')
+def send_js(path):
+        return send_from_directory('result', path)
 
 if __name__ == "__main__":
     app.run()
