b94e4347e7a0e424e3c7a82ea108e3f4671c0055
Tetsui Ohkubo <peryaudo@gmail.com>
2021-09-20 20:29:28 +0900

    Add debug pages

--- web.py
+++ web.py
@@ -9,6 +9,9 @@ DebugImage = namedtuple('DebugImage', ['tag', 'image', 'spread_idx'])
 with open('result/index.csv', 'r') as f:
     articles = list(map(Article._make, csv.reader(f)))
 
+with open('result/debug.csv', 'r') as f:
+    debug_images = list(map(DebugImage._make, csv.reader(f)))
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,6 +23,13 @@ def search():
     q = request.args.get('q')
     result = islice(filter(lambda article: q in article.heading, articles), 0, 25)
     return render_template('search.html', q=q, result=result)
+
+@app.route('/debug')
+def debug():
+    spread = request.args.get('spread')
+    tag = request.args.get('tag')
+    result = filter(lambda debug_image: debug_image.spread == spread and debug_image.tag == tag, debug_images)
+    return render_template('debug.html', spread=spread, tag=tag, result=result)
 
 @app.route('/images/<path:path>')
 def send_js(path):
