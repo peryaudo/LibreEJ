acff8db7f89d44a2198157f8b02e76049a0f31d9
Tetsui Ohkubo <peryaudo@gmail.com>
2021-09-24 15:11:06 +0900

    Count bad pages in stats.py. Closes #26
    Maybe it was not particularly a good idea...

--- stats.py
+++ stats.py
@@ -1,5 +1,6 @@
 import csv
 from csvtuples import Article
+import collections
 
 with open('result/index.csv', 'r') as f:
     articles = list(map(Article._make, csv.reader(f)))
@@ -8,6 +9,7 @@ print('The number of entries: %d' % len(articles))
 print('The number of entries with an empty heading: %d' %
         len([article for article in articles if article.heading == '']))
 
+bad_spreads = [article.spread_idx for article in articles if article.heading == '']
 articles = [article for article in articles if article.heading != '']
 print('The number of duplicate entries (except empty ones): %d' %
         (len(articles) - len(set(map(lambda article: article.heading, articles)))))
@@ -19,8 +21,14 @@ for i in range(len(articles) - 1):
 
     if not (a < b):
         count += 1
+        bad_spreads.append(articles[i].spread_idx)
 
 print('The number of entries that are not in dictionary order: %d' % count)
+
+print('\nTop 10 pages with the most entries:')
+counter = collections.Counter(bad_spreads)
+for spread_idx, count in counter.most_common(10):
+    print("  %s: %d entries" % (spread_idx, count))
 
 # TODO: The number of entries with a heading that the spell checker says is wrong
 # TODO: The number of cropped columns that have an unusual number of lines
