6d3e3aae630fbf332e15494bc2c1d44a622e1f54
Tetsui Ohkubo <peryaudo@gmail.com>
2021-09-26 19:50:38 +0900

    Show empty entries in /bad

--- web.py
+++ web.py
@@ -43,19 +43,23 @@ def debug():
     result = []
     debug_result = []
     if tag == 'article':
-        result = filter(lambda article: article.spread_idx == spread, articles)
+        result = filter(lambda article: article[0].spread_idx == spread, zip(articles, bodies))
     else:
         debug_result = filter(lambda debug_image: debug_image.spread_idx == spread and debug_image.tag == tag, debug_images)
     return render_template('debug.html', spread=int(spread), tag=tag, debug_result=debug_result, result=result)
 
 @app.route('/bad')
 def bad():
-    result = []
-    for i in range(len(articles) - 1):
-        a = articles[i].heading.lower()
-        b = articles[i + 1].heading.lower()
-        if not (a < b):
-            result.append(articles[i])
+    mode = request.args.get('mode')
+    if mode == 'empty':
+        result = list(filter(lambda article: article.heading == '', articles))
+    else:
+        result = []
+        for i in range(len(articles) - 1):
+            a = articles[i].heading.lower()
+            b = articles[i + 1].heading.lower()
+            if not (a < b):
+                result.append(articles[i])
     return render_template('bad.html', total=len(result), result=result[:200])
 
 @app.route('/images/<path:path>')
